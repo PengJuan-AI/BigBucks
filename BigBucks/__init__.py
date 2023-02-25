bb3c5eb34535cba26349edb082d4a318026bf9e7
macbookp <jp584@duke.edu>
2023-02-25 13:33:49 -0500

    add index html

--- BigBucks/__init__.py
+++ BigBucks/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask,render_template
 
 def create_app(test_config=None):
     # create and configure the app
@@ -22,5 +22,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
     return app
