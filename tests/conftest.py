e815fc9172448941f1482f0713e59ef39036aa68
macbookp <jp584@duke.edu>
2023-03-17 12:25:13 -0400

    test db

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from BigBucks import create_app
 from BigBucks.db import get_db, init_db
 
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+with open(os.path.join(os.path.dirname(__file__), 'test_data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
 
 @pytest.fixture
---
+++ tests/test_db.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+import pytest
+from flaskr.db import get_db
+
+# Should return the same connection each time it's called.
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+        
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT user1')
+        
+    assert 'closed' in str(e.value)
+
+# the init-db command should call the init_db function and output a message
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    monkeypatch.setattr('BigBucks.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
