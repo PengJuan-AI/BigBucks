b345765732a8d9a7e56c8e13a2975051bd57f2a3
yf155 <yf155@duke.edu>
2023-03-21 13:58:06 -0400

    store outstanding shares in database

--- BigBucks/static_data_processor.py
+++ BigBucks/static_data_processor.py
@@ -13,29 +13,26 @@ def read_stock_data(filename):
         data = [(row[0], float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5]), int(row[6])) for row in reader]
     return data
 
-#data = read_stock_data('AAPL.csv')
-#print(data)
-    
 # Add asset to Assets_info table and get its assetid
 def add_asset(conn, symbol):
     c = conn.cursor()
     # Check if asset already exists in table
-    c.execute("SELECT assetid, name FROM Assets_info WHERE symbol=?", (symbol,))
+    c.execute("SELECT assetid, name, shares FROM Assets_info WHERE symbol=?", (symbol,))
     asset = c.fetchone()
     if asset is not None:
-        assetid = asset[0]
-        # Update the company name if it has changed
-        new_company_name = get_company_name(symbol)
-        if new_company_name != asset[1]:
-            c.execute("UPDATE Assets_info SET name=? WHERE assetid=?", (new_company_name, assetid))
-            conn.commit()
-        return assetid, new_company_name
-    # If asset doesn't exist, get company name from Yahoo Finance and insert into table
+        assetid, company_name, shares = asset
+        # Update shares if asset already exists
+        shares_outstanding = get_company_shares(symbol)
+        c.execute("UPDATE Assets_info SET shares=? WHERE assetid=?", (shares_outstanding, assetid))
+        conn.commit()
+        return assetid, company_name, shares_outstanding
+    # If asset doesn't exist, get company name and shares from Yahoo Finance and insert into table
     company_name = get_company_name(symbol)
-    c.execute("INSERT INTO Assets_info (symbol, name) VALUES (?, ?)", (symbol, company_name))
+    shares_outstanding = get_company_shares(symbol)
+    c.execute("INSERT INTO Assets_info (symbol, name, shares) VALUES (?, ?, ?)", (symbol, company_name, shares_outstanding))
     assetid = c.lastrowid
     conn.commit()
-    return assetid, company_name
+    return assetid, company_name, shares_outstanding
 
 # Add stock data to Assets_data table
 def add_stock_data(conn, assetid, stock_data):
@@ -51,6 +48,14 @@ def get_company_name(symbol):
     company_name = json.loads(content.decode('utf8'))['quotes'][0]['shortname']
     return company_name
 
+#Get company's outstanding shares by its symbol from Yahoo Finance
+def get_company_shares(symbol):
+    response = urllib.request.urlopen(f'https://query2.finance.yahoo.com/v10/finance/quoteSummary/{symbol}?modules=assetProfile,defaultKeyStatistics,financialData')
+    content = response.read()
+    data = json.loads(content.decode('utf8'))
+    shares_outstanding = data['quoteSummary']['result'][0]['defaultKeyStatistics']['sharesOutstanding']['raw']
+    return shares_outstanding
+
 # Open database connection
 db_path = os.path.join(os.path.dirname(__file__), 'BigBucks.db')
 conn = sqlite3.connect(db_path)
@@ -60,7 +65,7 @@ for filename in os.listdir('static/stockdata'):
     if not filename.endswith('.csv'):
         continue
     symbol = filename[:-4]  # Remove '.csv' extension
-    assetid, company_name = add_asset(conn, symbol)
+    assetid, company_name, shares_outstanding = add_asset(conn, symbol)
     stock_data = read_stock_data(filename)
     add_stock_data(conn, assetid, stock_data)
 
