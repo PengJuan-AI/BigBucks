0f7d189f6a10eb5a7e6ff0e54500ec9ad6ea822a
macbookp <jp584@duke.edu>
2023-03-24 17:28:30 -0400

    change assetid to symbol

--- BigBucks/order.py
+++ BigBucks/order.py
@@ -25,16 +25,16 @@ def buy():
         error = None
         balance = get_balance(id)
         amount = price*shares_traded
-        assetid = get_assetid(symbol)
-        shares_owned = get_shares(id, assetid)
+        # assetid = get_assetid(symbol)
+        shares_owned = get_shares(id, symbol)
 
         # check balance
         if balance< amount:
             error = "Balance is not enough"
         else:
-            buy_asset(id,assetid, balance, amount, shares_traded, shares_owned)
+            buy_asset(id,symbol, balance, amount, shares_traded, shares_owned)
             # update_asset_info(assetid, shares_traded)
-            update_orders(date,id, assetid, shares_traded, price, action )
+            update_orders(date,id, symbol, shares_traded, price, action )
 
         flash(error)
 
@@ -60,15 +60,15 @@ def sell():
         error = None
         amount = price*shares_traded
         balance = get_balance(id)
-        assetid = get_assetid(symbol)
-        shares_owned = get_shares(id, assetid)
+        # assetid = get_assetid(symbol)
+        shares_owned = get_shares(id, symbol)
 
         if shares_traded>shares_owned:
             error = 'Shares owned are not enough'
         else:
-            sell_asset(id,assetid,balance, amount, shares_traded, shares_owned)
+            sell_asset(id,symbol,balance, amount, shares_traded, shares_owned)
             # update_asset_info(assetid, -shares_traded)
-            update_orders(date, id, assetid, shares_traded, price, action)
+            update_orders(date, id, symbol, shares_traded, price, action)
 
         flash(error)
         return redirect(url_for('index'))
@@ -90,17 +90,17 @@ def get_balance(id):
     
     return balance
 
-def get_assetid(symbol):
-    id = get_db().execute(
-        'SELECT assetid FROM Assets_info WHERE symbol = ?', (symbol,)
-    ).fetchone()[0]
-
-    return id
+# def get_assetid(symbol):
+#     id = get_db().execute(
+#         'SELECT assetid FROM Assets_info WHERE symbol = ?', (symbol,)
+#     ).fetchone()[0]
+# 
+#     return id
 
 # use functions from data_processor
-def get_shares(userid, assetid):
+def get_shares(userid, symbol):
     shares = get_db().execute(
-        'SELECT shares FROM portfolio WHERE userid = ? and assetid=?', (userid, assetid)
+        'SELECT shares FROM portfolio WHERE userid = ? and symbol=?', (userid, symbol)
     ).fetchone()
     if shares is None:
         return 0
@@ -113,19 +113,19 @@ def get_outstanding(symbol):
 def get_asset_name(symbol):
     return get_company_name(symbol)
 
-def buy_asset(userid,assetid,balance, amount, shares_traded, shares_owned):
+def buy_asset(userid,symbol,balance, amount, shares_traded, shares_owned):
     '''
     buy asset: Portfoio change (shares increase), Balance change (balance decrease)
     '''
     db = get_db()
     # if the asset is not in portfolio, insert into portfolio, else update shares of the asset in portfolio
     if shares_owned == 0:
-        db.execute("INSERT INTO portfolio (userid, assetid, shares) VALUES (?,?,?)",
-                   (userid, assetid, shares_traded)
+        db.execute("INSERT INTO portfolio (userid, symbol, shares) VALUES (?,?,?)",
+                   (userid, symbol, shares_traded)
                    )
     else:
-        db.execute("UPDATE portfolio SET shares=? WHERE assetid=? and userid=?",
-                   (shares_owned+shares_traded, assetid, userid)
+        db.execute("UPDATE portfolio SET shares=? WHERE symbol=? and userid=?",
+                   (shares_owned+shares_traded, symbol, userid)
                    )
     
     # Deduct amount from user's balance
@@ -133,12 +133,12 @@ def buy_asset(userid,assetid,balance, amount, shares_traded, shares_owned):
     
     db.commit()
         
-def sell_asset(userid,assetid,balance, amount, shares_traded, shares_owned):
+def sell_asset(userid,symbol,balance, amount, shares_traded, shares_owned):
     print('In sell asset')
     db = get_db()
     
-    db.execute("UPDATE portfolio SET shares=? WHERE assetid=? and userid=?",
-               (shares_owned-shares_traded, assetid, userid) )
+    db.execute("UPDATE portfolio SET shares=? WHERE symbol=? and userid=?",
+               (shares_owned-shares_traded, symbol, userid) )
 
     # Deduct amount from user's balance
     db.execute("UPDATE balance SET balance=? WHERE userid=?", (balance + amount, userid))
@@ -146,21 +146,21 @@ def sell_asset(userid,assetid,balance, amount, shares_traded, shares_owned):
     db.commit()
 
 # unnecessary in next step
-def update_asset_info(assetid, shares_traded):
-    '''
-    after one asset is traded, the outstanding shares of it decrease
-    '''
-    db = get_db()
-    outstanding = db.execute('SELECT shares FROM assets_info WHERE assetid=?', (assetid,)).fetchone()[0]
-    db.execute('UPDATE assets_info set shares=? WHERE assetid=?',
-               (outstanding-shares_traded, assetid)
-    )
+# def update_asset_info(assetid, shares_traded):
+#     '''
+#     after one asset is traded, the outstanding shares of it decrease
+#     '''
+#     db = get_db()
+#     outstanding = db.execute('SELECT shares FROM assets_info WHERE assetid=?', (assetid,)).fetchone()[0]
+#     db.execute('UPDATE assets_info set shares=? WHERE assetid=?',
+#                (outstanding-shares_traded, assetid)
+#     )
 
 
-def update_orders(date,id, assetid, shares, price, action ):
+def update_orders(date,id, symbol, shares, price, action ):
     db = get_db()
-    db.execute('INSERT INTO orders (order_date, userid, assetid, quantity, price, action) VALUES (?,?,?,?,?,?)',
-               (date, id, assetid,shares, price, action))
+    db.execute('INSERT INTO orders (order_date, userid, symbol, quantity, price, action) VALUES (?,?,?,?,?,?)',
+               (date, id, symbol,shares, price, action))
     
 # test
 @bp.route('/get_stock_info', methods=['POST'])
