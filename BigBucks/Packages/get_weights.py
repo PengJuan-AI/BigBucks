05585d68a43b5b8d1712b9488b4a356d71ebca23
macbookp <jp584@duke.edu>
2023-03-27 22:37:24 -0400

    get asset weights in portfolio

---
+++ BigBucks/Packages/get_weights.py
@@ -0,0 +1,14 @@
+from BigBucks.db import get_db
+
+def get_portfolio_weights(id):
+    weights = {}
+    db = get_db()
+    pf = db.execute("SELECT symbol, value from portfolio WHERE userid=?",(id,))
+    total_value = db.execute("SELECT sum(value) from portfolio GROUP BY userid "
+                             "Having userid=?", (id,)).fetchone()[0]
+    
+    for p in pf:
+        weights[p[0]] = round(p[1]/total_value,2)
+    print(weights)
+    
+    return weights
---
+++ tests/test_analysis.py
@@ -0,0 +1,22 @@
+import pytest
+from flask import g, session
+from BigBucks.db import get_db
+from BigBucks.order import get_balance, buy_asset, get_company_name,get_company_shares
+from live_data_processor import get_live_price
+from Packages.get_weights import get_portfolio_weights
+
+def test_get_weights(client, app, auth):
+    auth.login()
+    
+    with app.app_context():
+        price1 = get_live_price('AAPL')
+        price2 = get_live_price('TSLA')
+        client.post('/order/buy',
+                    data={'symbol': 'AAPL', 'date': '2023-3-15', 'price': price1, 'share': 200, 'action': 'buy'})
+        client.post('/order/buy',
+                    data={'symbol': 'TSLA', 'date': '2023-3-15', 'price': price2, 'share': 200, 'action': 'buy'})
+        total_value = price1*200+price2*200
+        weights = get_portfolio_weights(1)
+        assert weights['AAPL'] == round(price1*200/total_value,2)
+        assert weights['TSLA']+weights['AAPL'] == 1
+        
