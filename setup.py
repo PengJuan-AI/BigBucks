43e0ebd6718a57894c3d63c21476686f5a265b87
macbookp <jp584@duke.edu>
2023-04-20 13:39:15 -0400

    setup ver.2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='BigBucks',
-    version='1.0.2',
+    version='2.0.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
